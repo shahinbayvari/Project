0ebdc6638b3a553cb032eadbe5398c5e757d5807
shahinbayvari <shahinbayvari@gmail.com>
2022-06-01 08:06:12 -0400

    Add JAXB code to Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            JAXBContext context = JAXBContext.newInstance(Order.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Order order = (Order) unmarshaller.unmarshal(
+                    new File("/home/kali/IdeaProjects/Project/src/main/resources/order.xml"));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 }
