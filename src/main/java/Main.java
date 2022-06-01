cfd8f4b27184f3e38252a1ec4c7aad65dadd2082
shahinbayvari <shahinbayvari@gmail.com>
2022-06-01 08:23:35 -0400

    Move JAXB code into orderCreator()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
+    private static Order orderCreator() {
+        Order order = null;
         try {
             JAXBContext context = JAXBContext.newInstance(Order.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            Order order = (Order) unmarshaller.unmarshal(
+            order = (Order) unmarshaller.unmarshal(
                     new File("/home/kali/IdeaProjects/Project/src/main/resources/order.xml"));
+
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+        return order;
+    }
+
+    public static void main(String[] args) {
+        Order order = orderCreator();
     }
 }
