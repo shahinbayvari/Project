efa00208b9f4b6b3512a0feef0388c525319f7d2
shahinbayvari <shahinbayvari@gmail.com>
2022-06-01 07:47:58 -0400

    Add global variables to Order.java

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,3 +1,24 @@
-public class Order {
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlRootElement(name = "order")
+@XmlType(propOrder = {
+        "id",
+        "naam",
+        "adres",
+        "postcode",
+        "plaats",
+        "provincie",
+        "land",
+        "productId"
+})
+public class Order {
+    private String id;
+    private String naam;
+    private String adres;
+    private String postcode;
+    private String plaats;
+    private String provincie;
+    private String land;
+    private String productId;
 }
