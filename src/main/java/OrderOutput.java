db215d94f1cda766f7967528c50ce47b00d8b847
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:14:02 -0400

    Change global variables to protected

--- src/main/java/OrderOutput.java
+++ src/main/java/OrderOutput.java
@@ -1,7 +1,7 @@
 public class OrderOutput {
-    private String orderReference;
-    private String nawGegevens;
-    private String productId;
+    protected String orderReference;
+    protected String nawGegevens;
+    protected String productId;
 
     public OrderOutput(String orderReference, String nawGegevens, String productId) {
         this.orderReference = orderReference;
