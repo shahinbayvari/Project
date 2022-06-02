e0a93ed725d80198ff254cbe8d1127c1c3912875
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:46:27 -0400

    Add global variables orderInput and orderOutput

--- src/test/java/TestOrder.java
+++ src/test/java/TestOrder.java
@@ -2,20 +2,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestOrder {
+    private OrderInput orderInput = Main.orderCreator();
+    private OrderOutput orderOutput = Main.convertToJSON(orderInput);
     @Test
-    public void testOrderId(OrderOutput orderOutput) {
+    public void testOrderId() {
         String orderReference = "12345";
         assertEquals(orderReference, orderOutput.orderReference);
     }
 
     @Test
-    public void testNawGegevens(OrderOutput orderOutput) {
+    public void testNawGegevens() {
         String nawGegevens = "Jantje Jantjessen\nJantje Jantjessenstraat 5A\n9183DE Jantjesstad\nNederland";
         assertEquals(nawGegevens, orderOutput.nawGegevens);
     }
 
     @Test
-    public void testProductId(OrderOutput orderOutput) {
+    public void testProductId() {
         String productId = "83748";
         assertEquals(productId, orderOutput.productId);
     }
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,2 +1,13 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class TestRunner {
+    public static void main(String[] args) {
+        Result test = JUnitCore.runClasses(TestOrder.class);
+        for (Failure failure : test.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(test.wasSuccessful());
+    }
 }
