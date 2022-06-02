495c75266fa59e674bacb7b1a0f3b79338b1d168
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:22:56 -0400

    Add TestOrderId()

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class Test {
+    private static void testOrderId(OrderOutput orderOutput) {
+        String orderReference = "12345";
+        assertEquals(orderReference, orderOutput.orderReference);
+    }
     public static void main(String[] args) {
         OrderInput orderInput = Main.orderCreator();
         OrderOutput orderOutput = Main.convertToJSON(orderInput);
-
+        testOrderId(orderOutput);
     }
 }
