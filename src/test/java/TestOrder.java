36bf307a0a78b7e0b6fc2db926502b5bebbaa035
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:43:58 -0400

    Make all methods in TestOrder.java public

--- src/test/java/TestOrder.java
+++ src/test/java/TestOrder.java
@@ -3,19 +3,19 @@ import static org.junit.Assert.assertEquals;
 
 public class TestOrder {
     @Test
-    private void testOrderId(OrderOutput orderOutput) {
+    public void testOrderId(OrderOutput orderOutput) {
         String orderReference = "12345";
         assertEquals(orderReference, orderOutput.orderReference);
     }
 
     @Test
-    private void testNawGegevens(OrderOutput orderOutput) {
+    public void testNawGegevens(OrderOutput orderOutput) {
         String nawGegevens = "Jantje Jantjessen\nJantje Jantjessenstraat 5A\n9183DE Jantjesstad\nNederland";
         assertEquals(nawGegevens, orderOutput.nawGegevens);
     }
 
     @Test
-    private void testProductId(OrderOutput orderOutput) {
+    public void testProductId(OrderOutput orderOutput) {
         String productId = "83748";
         assertEquals(productId, orderOutput.productId);
     }
