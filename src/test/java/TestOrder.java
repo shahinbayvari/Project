b239115e9cd49be929d5837415bb74b041c35a2f
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:35:29 -0400

    Add testOrderId, testNawGegevens and testProductId

--- src/test/java/TestOrder.java
+++ src/test/java/TestOrder.java
@@ -1,13 +1,22 @@
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestOrder {
-    private static void testOrderId(OrderOutput orderOutput) {
+    @Test
+    private void testOrderId(OrderOutput orderOutput) {
         String orderReference = "12345";
         assertEquals(orderReference, orderOutput.orderReference);
     }
-    public static void main(String[] args) {
-        OrderInput orderInput = Main.orderCreator();
-        OrderOutput orderOutput = Main.convertToJSON(orderInput);
-        testOrderId(orderOutput);
+
+    @Test
+    private void testNawGegevens(OrderOutput orderOutput) {
+        String nawGegevens = "Jantje Jantjessen\nJantje Jantjessenstraat 5A\n9183DE Jantjesstad\nNederland";
+        assertEquals(nawGegevens, orderOutput.nawGegevens);
+    }
+
+    @Test
+    private void testProductId(OrderOutput orderOutput) {
+        String productId = "83748";
+        assertEquals(productId, orderOutput.productId);
     }
 }
