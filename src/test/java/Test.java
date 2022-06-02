b7b2af6b03a777cf6ed8b8a0695cac5a1f063468
shahinbayvari <shahinbayvari@gmail.com>
2022-06-02 07:14:50 -0400

    Create orderInput and orderOutput in Test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,7 @@
 public class Test {
+    public static void main(String[] args) {
+        OrderInput orderInput = Main.orderCreator();
+        OrderOutput orderOutput = Main.convertToJSON(orderInput);
 
+    }
 }
